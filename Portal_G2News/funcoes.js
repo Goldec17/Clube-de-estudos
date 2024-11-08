2f88b2e1a3f6e8408b3a7b74c010b44f0368df66
Goldec17 <134340829+Goldec17@users.noreply.github.com>
2024-11-08 07:14:44 -0300

    Add files via upload

---
+++ Portal_G2News/funcoes.js
@@ -0,0 +1,10 @@
+function mudaFoto(foto){
+    document.getElementById("anuncio1").src = foto ;
+}
+function mudaFoto2(foto){
+    document.getElementById("anuncio2").src = foto ;
+}
+function mudaFoto3(foto){
+    document.getElementById("anuncio3").src = foto ;
+}
+
